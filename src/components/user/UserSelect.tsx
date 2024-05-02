ffd6bd1fe926777aa885e740f5353b863375f382
congnv3030 <congnv3030@gmail.com>
2024-05-02 17:08:14 +0700

    update: user select component

--- src/components/user/UserSelect.tsx
+++ src/components/user/UserSelect.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useState } from 'react';
+import { forwardRef, useImperativeHandle } from 'react';
 import clsx from 'clsx';
 
 import { IUser } from '@/types';
@@ -44,42 +44,54 @@ const users: IUser[] = [
   },
 ];
 
-export const UserSelect = forwardRef((props, ref) => {
-  const [user, setUser] = useState<IUser | null>(null);
+export interface UserSelectProps {
+  value?: IUser | null;
+  onChange?: (value: IUser | null) => void;
+}
 
-  return (
-    <Select<IUser>
-      by="id"
-      value={user}
-      className="cursor-pointer bg-div-bound text-sm font-light leading-4.5"
-      contentClassName="w-80 max-w-full space-y-2"
-      renderSelectedOption={(option) => option.name}
-      onChange={setUser}
-    >
-      {users.map((user) => (
-        <SelectOption
-          key={user.id}
-          value={user}
-          className={({ isSelected }) =>
-            clsx(
-              'flex min-h-12.5 items-center rounded-lg px-3 py-2 transition-colors hover:bg-menu-item',
-              {
-                'bg-menu-item': isSelected,
-              },
-            )
-          }
-        >
-          <div className="pr-4">
-            <img src={user.avatar_url} className="h-8 w-8" alt={user.name} />
-          </div>
-          <div className="text-left">
-            <p className="text-sm font-light leading-4.5">{user.name}</p>
-            <p className="mt-1 text-xs font-light leading-3 text-sub">
-              @{user.username}
-            </p>
-          </div>
-        </SelectOption>
-      ))}
-    </Select>
-  );
-});
+export interface UserSelectRef {}
+
+export const UserSelect = forwardRef<UserSelectRef, UserSelectProps>(
+  ({ value, onChange }, ref) => {
+    const renderSelectedOption = (option: IUser) => option.name;
+
+    useImperativeHandle(ref, () => ({}));
+
+    return (
+      <Select<IUser>
+        by="id"
+        placeholder="Paste, scan or select recipient"
+        value={value}
+        className="cursor-pointer bg-div-bound text-sm font-light leading-4.5"
+        contentClassName="w-80 max-w-full space-y-2"
+        renderSelectedOption={renderSelectedOption}
+        onChange={onChange}
+      >
+        {users.map((user) => (
+          <SelectOption
+            key={user.id}
+            value={user}
+            className={({ isSelected }) =>
+              clsx(
+                'flex min-h-12.5 items-center rounded-lg px-3 py-2 transition-colors hover:bg-menu-item',
+                {
+                  'bg-menu-item': isSelected,
+                },
+              )
+            }
+          >
+            <div className="pr-4">
+              <img src={user.avatar_url} className="h-8 w-8" alt={user.name} />
+            </div>
+            <div className="text-left">
+              <p className="text-sm font-light leading-4.5">{user.name}</p>
+              <p className="mt-1 text-xs font-light leading-3 text-sub">
+                @{user.username}
+              </p>
+            </div>
+          </SelectOption>
+        ))}
+      </Select>
+    );
+  },
+);
