b97b1d26882f8cd4bf31b7a30fa9a1d9d1fefc39
congnv3030 <congnv3030@gmail.com>
2024-05-02 10:32:14 +0700

    Initial project

---
+++ README.md
@@ -0,0 +1,33 @@
+# Send Crypto Form Practice
+
+## Installation
+
+Install package
+```
+npm install
+```
+
+## Available Scripts
+
+Runs the app in the development mode
+```
+npm run dev
+```
+
+Builds the app for production to the `dist` folder
+```
+npm run build
+```
+
+Runs the app in the production mode. You need run this command first `npm run build`
+```
+npm run preview
+```
+
+Find and fix problems in your JavaScript code - ESLint
+```
+npm run lint
+```
+```
+npm run lint:fix
+```
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    assetsDir: 'static',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'static/[hash].chunk.js',
+        assetFileNames: 'static/[hash].chunk.[ext]',
+      },
+    },
+  },
+});
